e7dc0b1a606410ae210d213c707cbbaa97f9f6da
Pablo Rojas <pablorojasa@guanajuato.gob.mx>
2023-07-14 12:23:38 -0600

    Angular Routing - 16 Precarga de modulos

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -25,8 +24,8 @@ const routes: Routes = [
         component: HomeComponent
       },
       {
-        path: 'category/:idCategory',
-        component: CategoryComponent
+        path: 'category',
+        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule)
       },
       {
         path: 'product/:idProducto',
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SwiperModule } from 'swiper/angular';
 
 import { WebsiteRoutingModule } from './website-routing.module';
+import { SharedModule } from './../shared/shared.module';
 
-import { ImgComponent } from './components/img/img.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pibes/reverse.pipe';
-import { TimeAgoPipe } from './pibes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -23,15 +17,8 @@ import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
     HomeComponent,
-    CategoryComponent,
     MycartComponent,
     LoginComponent,
     RegisterComponent,
@@ -44,6 +31,7 @@ import { LayoutComponent } from './components/layout/layout.component';
     CommonModule,
     WebsiteRoutingModule,
     SwiperModule, // Slide
+    SharedModule
   ]
 })
 export class WebsiteModule { }
